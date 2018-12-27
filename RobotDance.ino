0a08cbd941e46c9d454eded07bc02edda6b10a82
martinerk0 <martiner08@gmail.com>
2018-12-27 13:24:58 +0100

    added parsing states and unfinished parsing functions

--- RobotDance.ino
+++ RobotDance.ino
@@ -14,60 +14,94 @@ enum turning_direction
   right
 }
 
+enum parsing_input{
+  start_state,
+  reading_starting_position_state,
+  reading_position_state,
+  reading_time_state,
+  malformed_input_state
+}
+
+typedef struct coordinate{
+    char first; //x-coordinate
+    char second; //y-coordinate
+//    unsigned long wait; //when to move next
+};
+
 typedef struct choreography{
   char stratringOrientation;
   coordinate startingPosition;
-  [] coordinate choreographyArray;   // does not contain starting position!
-}
+  coordinate [] choreographyArray;   // does not contain starting position!
+};
 
-typedef struct coordinate
-  {
-      char first; //x-coordinate
-      char second; //y-coordinate
-      unsigned long wait; //when to move next
-  };
 
 // TODO: setEPROM, getEprom (zapis a potom nacitaj choreografiu)
 // zresetovat eepromku,
-void parse_input_to_coordinate(string input_file){}
-void turn_in_place(turning_direction direction, number_of_times){}
-void go_straight(number_of_fields) {}
-void go_to_coordinate(coordinate initial_coordinate, coordinate target_coordinate){}
-void go_to_start_position()
+//void parse_input_to_coordinate(string input_file){}
+//void turn_in_place(turning_direction direction, number_of_times){}
+//void go_straight(number_of_fields) {}
+//void go_to_coordinate(coordinate initial_coordinate, coordinate target_coordinate){}
+//void go_to_start_position(){}
 
+parsing_input parsing_input_state;
 coordinate start_position;
 coordinate current_position;
-coordinate[] dance_choreography;
+coordinate [] dance_choreography;
 
 void setup() {
   // TODO: reset eeprom and write default dance here to eeprom
 
 }
 
-void handleSerial() {
- while (Serial.available() > 0) {
-   char incomingCharacter = serial.read();
-   switch (incomingCharacter) {
-     case ‘+’:
-      pwmValue = pwmValue + 5;
-      If (pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
- 
-     case ‘-’:
-      pwmValue = pwmValue - 5;
-      If (pwmValue <= 0)
-         pwmValue = 0;
-      break;
-    }
- }
+
+/**
+ * Can contain:
+ * starting position:      A1N
+ * position without time:  C4 or 4C
+ * time:                   T350
+ */
+//boolean validateInputToken(char [] chars){
+//   return true;
+//}
+
+boolean handleSerial() {
+    while (Serial.available() > 0) {
+//        [] cha
+        char incomingCharacter = serial.read();
+        switch(robot_state){
+            case parsing_input_state:
+                //mozem citat lubovolne whitespacy, ale akonahle bude char, chod do reading reading_starting_position_state
+                break;
+            case reading_starting_position_state:
+                // citaj chary, ale akonahle bude whitespace, zvaliduj position a chod bud do malformed alebo do reading_position_state
+                break;
+            case reading_position_state:
+                // citaj chary, ale akonahle je whitespace zvaliduj, a chod bud do reading_time_stat alebo do  malformed_input_state
+                break;
+            case reading_time_stat:
+                // citaj chary, ale akonahle je whitespace zvaliduj, a chod do reading_position_state alebo malformed_input_state
+                break;    
+            case malformed_input_state:
+                // prestan citat zo serial liny
+                break;    
+        }
+   }
+
+   
 }
 
 // main loop
 void loop() {
     switch(robot_state){
       case waiting_for_start_state:
-          handleSerial();
+          boolean useCustomChoreography = handleSerial();
+          if(useCustomChoreography){
+            // do dance with custom choreography
+          }
+          else{
+            // do dance with default choreography
+          }
+          
           break;
       case doing_choreography_state:
           break;
